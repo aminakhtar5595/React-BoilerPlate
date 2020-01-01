2116e10ee9cfd9b7b35ea89aa611122ddeb96036
Amin Akhtar <48668290+aminakhtar5595@users.noreply.github.com>
2020-01-01 14:58:15 +0500

    Add files via upload

---
+++ app/Constants/index.js
@@ -0,0 +1,10 @@
+module.exports = {
+    // baseURL: 'http://api.bms.dwtdemo.com/api/', // For Stagging
+    baseURL: 'http://api.findabait.com/api/', // For Live
+
+    // currentVersion: 'v0.1.3',
+
+
+    // google_api_key: 'AIzaSyDMKXgTik_BboalQAMEVvHdUBoPqfQ6FTc'
+
+}
